4464add75d4cb8d433ea10ade5ca3a199d478515
Fuyu <F-uyu@github.com>
2024-04-15 23:36:08 -0700

    Added GUI

--- client.py
+++ client.py
@@ -1,62 +1,134 @@
+# import required modules
 import socket
 import threading
-#test
-# define the local host and port
-HOST = "127.0.0.1"
+import tkinter as tk
+from tkinter import scrolledtext
+from tkinter import messagebox
+
+HOST = '127.0.0.1'
 PORT = 1738
 
-# listen for message from server
-def listen_for_messages_from_server(client):
-    while 1:
+DARK_GREY = '#121212'
+MEDIUM_GREY = '#1F1B24'
+OCEAN_BLUE = '#464EB8'
+WHITE = "white"
+FONT = ("Helvetica", 17)
+BUTTON_FONT = ("Helvetica", 15)
+SMALL_FONT = ("Helvetica", 13)
 
-        message = client.recv(2048).decode('utf-8')
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-        if message != '':
-            username = message.split(":")[0]
-            content = message.split(':')[1]
+def show_emoticons():
+    # Create a Toplevel window
+    emoticons_window = tk.Toplevel(root)
+    emoticons_window.geometry("200x400")
+    emoticons_window.title("Emoticons")
 
-            print(f"[{username}] {content}")
-        else:
-            print("The message recevived is empty.")
+    # Create a list of emoticons
+    emoticons = ["😊", "😂", "😍", "😎", "😜", "😇", "😘", "🥳", "🤩"]
+    def add_to_message(emote):
+        message_textbox.insert(tk.END, emote)
+    # Display the list of emoticons
+    for emoticon in emoticons:
+        button = tk.Button(emoticons_window, text=emoticon, font=("Arial", 12), command=lambda e=emoticon: add_to_message(e))
+        button.pack()
 
-# sends message to server
-def send_message_to_server(client):
-    while 1:
-        message = input("Message: ")
-        if message != '':
-            client.sendall(message.encode())
-        else:
-            print("Message is empty.")
-            exit(0)
 
-# commuicates to the server
-def communicate_to_server(client):
-    username = input("Enter username: ")
+def add_message(message):
+    message_box.config(state=tk.NORMAL)
+    message_box.insert(tk.END, message + '\n')
+    message_box.config(state=tk.DISABLED)
+
+def connect():
+
+    # try except block
+    try:
+
+        # Connect to the server
+        client.connect((HOST, PORT))
+        print("Successfully connected to server")
+        add_message("[SERVER] Successfully connected to the server")
+    except:
+        messagebox.showerror("Unable to connect to server", f"Unable to connect to server {HOST} {PORT}")
+
+    username = username_textbox.get()
     if username != '':
         client.sendall(username.encode())
     else:
-        print("Username cannot be empty.")
-        exit(0)
-    
+        messagebox.showerror("Invalid username", "Username cannot be empty")
+
     threading.Thread(target=listen_for_messages_from_server, args=(client, )).start()
 
-    send_message_to_server(client)
+    username_textbox.config(state=tk.DISABLED)
+    username_button.config(state=tk.DISABLED)
+
+def send_message():
+    message = message_textbox.get()
+    if message != '':
+        client.sendall(message.encode())
+        message_textbox.delete(0, len(message))
+    else:
+        messagebox.showerror("Empty message", "Message cannot be empty")
+
+root = tk.Tk()
+root.geometry("600x600")
+root.title("Messenger Client")
+root.resizable(False, False)
+
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=4)
+root.grid_rowconfigure(2, weight=1)
+
+top_frame = tk.Frame(root, width=600, height=100, bg=DARK_GREY)
+top_frame.grid(row=0, column=0, sticky=tk.NSEW)
+
+middle_frame = tk.Frame(root, width=600, height=400, bg=MEDIUM_GREY)
+middle_frame.grid(row=1, column=0, sticky=tk.NSEW)
+
+bottom_frame = tk.Frame(root, width=600, height=100, bg=DARK_GREY)
+bottom_frame.grid(row=2, column=0, sticky=tk.NSEW)
+
+username_label = tk.Label(top_frame, text="Enter name:", font=FONT, bg=DARK_GREY, fg=WHITE)
+username_label.pack(side=tk.LEFT, padx=10)
+
+username_textbox = tk.Entry(top_frame, font=FONT, bg=MEDIUM_GREY, fg=WHITE, width=23)
+username_textbox.pack(side=tk.LEFT)
+
+username_button = tk.Button(top_frame, text="Connect", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, command=connect)
+username_button.pack(side=tk.LEFT, padx=15)
+
+message_textbox = tk.Entry(bottom_frame, font=FONT, bg=MEDIUM_GREY, fg=WHITE, width=35)
+message_textbox.pack(side=tk.LEFT, padx=10)
+
+emotes_frame = tk.Button(bottom_frame, text="😊", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, command=show_emoticons)
+emotes_frame.pack(side=tk.LEFT)
+
+message_button = tk.Button(bottom_frame, text="Send", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, command=send_message)
+message_button.pack(side=tk.LEFT, padx=10)
+
+message_box = scrolledtext.ScrolledText(middle_frame, font=SMALL_FONT, bg=MEDIUM_GREY, fg=WHITE, width=67, height=26.5)
+message_box.config(state=tk.DISABLED)
+message_box.pack(side=tk.TOP)
+
+
+def listen_for_messages_from_server(client):
+
+    while 1:
+
+        message = client.recv(2048).decode('utf-8')
+        if message != '':
+            username = message.split("~")[0]
+            content = message.split('~')[1]
+
+            add_message(f"[{username}] {content}")
+            
+        else:
+            messagebox.showerror("Error", "Message recevied from client is empty")
 
 # main function
 def main():
 
-    # create socket object (IPv4, TCP) & bind the client to the host and port
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-
-    # connect to the server
-    try:
-        client.connect((HOST,PORT))
-        print("Successfully connected to server.")
-    except:
-        print("Unable to connect to the server {HOST} {PORT}")
+    root.mainloop()
     
-    communicate_to_server(client)
-
-# calling main
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,71 +1,86 @@
+# Import required modules
 import socket
 import threading
 
-# define the local host and port
-HOST = "127.0.0.1"
+HOST = '127.0.0.1'
 PORT = 1738
 LISTENER_LIMIT = 5
-# list of all connected clients
-active_clients = [] 
+active_clients = [] # List of all currently connected users
 
-# listen to messages from the clients
+# Function to listen for upcoming messages from a client
 def listen_for_messages(client, username):
-    while 1:
-        message = client.recv(2048).decode('utf-8')
-        # check if the message is empty
-        if message != '':
-            final_msg = username + ": " + message
-            send_messages_to_all(final_msg)
-        else:
-            print(f"The message sent from client {username} is empty.")
 
-# send message to client
+    while 1:
+
+        message = client.recv(2048).decode('utf-8')
+        if message != '':
+            
+            final_msg = username + '~' + message
+            send_messages_to_all(final_msg)
+
+        else:
+            print(f"The message send from client {username} is empty")
+
+
+# Function to send message to a single client
 def send_message_to_client(client, message):
+
     client.sendall(message.encode())
 
-# sends messages to all connected clients 
+# Function to send any new message to all the clients that
+# are currently connected to this server
 def send_messages_to_all(message):
+    
     for user in active_clients:
+
         send_message_to_client(user[1], message)
 
-# handles clients
+# Function to handle client
 def client_handler(client):
     
-    # server listen for the client username
+    # Server will listen for client message that will
+    # Contain the username
     while 1:
+
         username = client.recv(2048).decode('utf-8')
-        # check if client username is empty
         if username != '':
             active_clients.append((username, client))
-            prompt_message = "SERVER: " + f"{username} added to the chat."
+            prompt_message = "SERVER~" + f"{username} added to the chat"
             send_messages_to_all(prompt_message)
             break
-        else: 
-            print("Client username is empty.")
+        else:
+            print("Client username is empty")
 
     threading.Thread(target=listen_for_messages, args=(client, username, )).start()
 
-# main function
+# Main function
 def main():
-    
-    # create socket object (IPv4, TCP) & bind the server to the host and port
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+
+    # Creating the socket class object
+    # AF_INET: we are going to use IPv4 addresses
+    # SOCK_STREAM: we are using TCP packets for communication
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Creating a try catch block
     try:
-        server.bind((HOST, PORT)) 
+        # Provide the server with an address in the form of
+        # host IP and port
+        server.bind((HOST, PORT))
         print(f"Running the server on {HOST} {PORT}")
     except:
-        print("Unable to bind to the host {HOST} and port {PORT}") 
-    
-    # number of maximum clients that can be connected
-    server.listen(LISTENER_LIMIT) 
+        print(f"Unable to bind to host {HOST} and port {PORT}")
 
-    # loop to accept incoming connections and to confirm connection
+    # Set server limit
+    server.listen(LISTENER_LIMIT)
+
+    # This while loop will keep listening to client connections
     while 1:
+
         client, address = server.accept()
         print(f"Successfully connected to client {address[0]} {address[1]}")
-    
-        threading.Thread(target=client_handler, args=(client, )).start() 
 
-# calling main
-if __name__ == "__main__":
+        threading.Thread(target=client_handler, args=(client, )).start()
+
+
+if __name__ == '__main__':
     main()
