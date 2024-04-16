d9dab38a3097048d8839f0e2424d5891e668ae7c
Fuyu <F-uyu@github.com>
2024-04-16 12:16:46 -0700

    Added type here...

--- client.py
+++ client.py
@@ -8,13 +8,13 @@ from tkinter import messagebox
 HOST = '127.0.0.1'
 PORT = 1738
 
-DARK_GREY = '#121212'
-MEDIUM_GREY = '#1F1B24'
-OCEAN_BLUE = '#464EB8'
+DARK_GREEN = '#22dc70'
+MEDIUM_ORANGE = '#f1b50e'
+OCEAN_BLUE = '#06d1fa'
 WHITE = "white"
 FONT = ("Helvetica", 17)
-BUTTON_FONT = ("Helvetica", 15)
-SMALL_FONT = ("Helvetica", 13)
+BUTTON_FONT = ("Lao UI", 15)
+SMALL_FONT = ("Lao UI", 13)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -70,6 +70,16 @@ def send_message():
     else:
         messagebox.showerror("Empty message", "Message cannot be empty")
 
+def clear_default_message(event):
+    if message_textbox.get() == "Type Here...":
+        message_textbox.delete(0, tk.END)
+        message_textbox.config(fg="black")  # Change text color when default message is cleared
+
+def restore_default_message(event):
+    if message_textbox.get() == "":
+        message_textbox.insert(0, "Type Here...")
+        message_textbox.config(fg="gray")  # Change text color for default message
+
 root = tk.Tk()
 root.geometry("600x600")
 root.title("Messenger Client")
@@ -79,25 +89,28 @@ root.grid_rowconfigure(0, weight=1)
 root.grid_rowconfigure(1, weight=4)
 root.grid_rowconfigure(2, weight=1)
 
-top_frame = tk.Frame(root, width=600, height=100, bg=DARK_GREY)
+top_frame = tk.Frame(root, width=600, height=100, bg=DARK_GREEN)
 top_frame.grid(row=0, column=0, sticky=tk.NSEW)
 
-middle_frame = tk.Frame(root, width=600, height=400, bg=MEDIUM_GREY)
+middle_frame = tk.Frame(root, width=600, height=400, bg=MEDIUM_ORANGE)
 middle_frame.grid(row=1, column=0, sticky=tk.NSEW)
 
-bottom_frame = tk.Frame(root, width=600, height=100, bg=DARK_GREY)
+bottom_frame = tk.Frame(root, width=600, height=100, bg=DARK_GREEN)
 bottom_frame.grid(row=2, column=0, sticky=tk.NSEW)
 
-username_label = tk.Label(top_frame, text="Enter name:", font=FONT, bg=DARK_GREY, fg=WHITE)
+username_label = tk.Label(top_frame, text="Enter name:", font=FONT, bg=DARK_GREEN, fg=WHITE)
 username_label.pack(side=tk.LEFT, padx=10)
 
-username_textbox = tk.Entry(top_frame, font=FONT, bg=MEDIUM_GREY, fg=WHITE, width=23)
+username_textbox = tk.Entry(top_frame, font=FONT, bg=MEDIUM_ORANGE, fg=WHITE, width=23)
 username_textbox.pack(side=tk.LEFT)
 
 username_button = tk.Button(top_frame, text="Connect", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, command=connect)
 username_button.pack(side=tk.LEFT, padx=15)
 
-message_textbox = tk.Entry(bottom_frame, font=FONT, bg=MEDIUM_GREY, fg=WHITE, width=35)
+message_textbox = tk.Entry(bottom_frame, font=FONT, bg=MEDIUM_ORANGE, fg=WHITE, width=35)
+message_textbox.insert(0, "Type Here...")
+message_textbox.bind("<FocusIn>", clear_default_message)
+message_textbox.bind("<FocusOut>", restore_default_message)
 message_textbox.pack(side=tk.LEFT, padx=10)
 
 emotes_frame = tk.Button(bottom_frame, text="😊", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, command=show_emoticons)
@@ -106,7 +119,7 @@ emotes_frame.pack(side=tk.LEFT)
 message_button = tk.Button(bottom_frame, text="Send", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, command=send_message)
 message_button.pack(side=tk.LEFT, padx=10)
 
-message_box = scrolledtext.ScrolledText(middle_frame, font=SMALL_FONT, bg=MEDIUM_GREY, fg=WHITE, width=67, height=26.5)
+message_box = scrolledtext.ScrolledText(middle_frame, font=SMALL_FONT, bg=MEDIUM_ORANGE, fg=WHITE, width=67, height=26.5)
 message_box.config(state=tk.DISABLED)
 message_box.pack(side=tk.TOP)
 
